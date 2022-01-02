564fa6a2a3c4ac77009f3ab5fa06cc9b2f4ea40a
mariorodriguez <mario.ra@students.salle.url.edu>
2022-01-02 18:51:34 +0100

    primeras tablas

--- src/testing.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS feeding;
-CREATE TABLE feeding (
-     farmer INTEGER,
-     animal INTEGER,
-     PRIMARY KEY (farmer)
-);
-INSERT INTO feeding VALUES (2, 3);
-SELECT * FROM feeding;
