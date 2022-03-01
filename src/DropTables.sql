6583aa23a2357958eeaae53e85218b3b36fa8eec
dserrabernad <d98serra@gmail.com>
2022-03-01 13:23:50 +0100

    File borrador de tablas

---
+++ src/DropTables.sql
@@ -0,0 +1,95 @@
+Drop Table If Exists Clan Cascade;
+
+Drop table if exists BatallaClan cascade; --> por rellenar
+
+Drop table if exists Lucha cascade; --> por rellenar
+
+Drop table if exists insignia cascade; --> rellenar
+
+Drop table if exists Adquiere cascade; -->Rellenar i preguntar como
+
+Drop table if exists Rol cascade;
+
+Drop table if exists Modificadors cascade;
+
+Drop table if exists Hacen cascade;
+
+Drop table if exists Jugador cascade;
+
+Drop table if exists Consigue cascade;
+
+Drop table if exists Formado cascade;
+
+Drop table if exists Dona cascade;
+
+DROP TABLE IF EXISTS Arena CASCADE;
+
+drop table if exists Carta cascade;
+
+Drop table if exists Mejoran cascade;
+
+Drop table if exists Tecnologia cascade;
+
+Drop table if exists Estructura cascade;
+
+Drop table if exists Depende cascade;
+
+DROP TABLE IF EXISTS Temporada CASCADE; --Season
+
+DROP TABLE IF EXISTS Participa CASCADE;
+
+DROP TABLE IF EXISTS Desbloquea CASCADE;
+
+DROP TABLE IF EXISTS Logro CASCADE;
+
+DROP TABLE IF EXISTS Obtiene CASCADE;
+
+DROP TABLE IF EXISTS Batalla_Jugadores CASCADE;
+
+DROP TABLE IF EXISTS Ocurre CASCADE;
+
+DROP TABLE IF EXISTS Mision CASCADE;
+
+DROP TABLE IF EXISTS Realiza CASCADE;
+
+DROP TABLE IF EXISTS Depende;
+
+drop table if exists Deck cascade;
+
+drop table if exists Ve cascade;
+
+drop table if exists Deck_carta cascade;
+
+drop table if exists Encuentra cascade;
+
+drop table if exists Luchan cascade;
+
+drop table if exists Edificio cascade;
+
+drop table if exists Tropas cascade;
+
+drop table if exists Encantamiento cascade;
+
+Drop table if exists Mensaje cascade;
+
+Drop table if exists Escribe cascade;
+
+Drop table if exists Amigo cascade;
+
+Drop table if exists Tarjeta cascade;
+
+Drop table if exists Posee cascade;
+
+Drop table if exists Articulo cascade;
+
+Drop table if exists Compra cascade;
+
+Drop table if exists Cofre cascade;
+
+Drop table if exists Paquete_Arena cascade;
+
+Drop table if exists Paquete_Oferta cascade;
+
+Drop table if exists Emoticono cascade;
+
+Drop table if exists Contiene cascade;
--- src/Tablas.sql
+++ src/Tablas.sql
@@ -1,95 +1,161 @@
-Drop Table If Exists Clan Cascade;
-
-Drop table if exists BatallaClan cascade; --> por rellenar
-
-Drop table if exists Lucha cascade; --> por rellenar
-
-Drop table if exists insignia cascade; --> rellenar
-
-Drop table if exists Adquiere cascade; -->Rellenar i preguntar como
-
-Drop table if exists Rol cascade;
-
-Drop table if exists Modificadors cascade;
-
-Drop table if exists Hacen cascade;
-
+/*------------ GUILLEM (VERDE)------------*/
 Drop table if exists Jugador cascade;
+Create table Jugador
+(
+    id          Varchar(50),
+    nombre      Varchar(50),
+    experiencia int,
+    trofeos     int,
+    oro         int,
+    gemas       int,
+    primary key (id)
+);
 
-Drop table if exists Consigue cascade;
+Drop Table If Exists Clan Cascade;
+Create Table Clan
+(
+    id             Varchar(25),
+    nombre         Varchar(50),
+    descripcion    Text,
+    trofeos_totales int,
+    minimo_trofeos  int,
+    puntuacion     int,
+    Primary Key (id)
+);
 
 Drop table if exists Formado cascade;
+Create table Formado
+(
+    id_clan     Varchar(50),
+    id_jugador  Varchar(50),
+    fecha date,
+    role Varchar(50),
+    primary key (id_clan, id_jugador),
+    foreign key (id_clan) references Clan (id),
+    foreign key (id_jugador) references Jugador (id)
+);
 
 Drop table if exists Dona cascade;
+Create table Dona
+(
+    id_clan        Varchar(50),
+    id_jugador     Varchar(50),
+    oro           int,
+    fecha          date,
+    primary key (id_clan, id_jugador),
+    foreign key (id_clan) references Clan (id),
+    foreign key (id_jugador) references Jugador (id)
+);
 
-DROP TABLE IF EXISTS Arena CASCADE;
+Drop table if exists Insignia cascade;
+Create table Insignia
+(
+    nombre        serial,           /*TODO: tipo?*/
+    imagenUrl     text,
+    primary key (nombre)
+);
 
-drop table if exists Carta cascade;
+Drop table if exists Adquiere cascade;
+Create table Adquiere
+(
+    id_clan     Varchar(50),
+    id_insignia int,
+    fecha       date,
+    primary key (id_clan, id_insignia),
+    foreign key (id_clan) references Clan (id),
+    foreign key (id_insignia) references Insignia (id)
+);
 
-Drop table if exists Mejoran cascade;
-
-Drop table if exists Tecnologia cascade;
-
-Drop table if exists Estructura cascade;
-
-Drop table if exists Depende cascade;
-
-DROP TABLE IF EXISTS Temporada CASCADE; --Season
+Drop table if exists Batalla_Clan cascade;
+Create table Batalla_Clan
+(
+    id         serial,
+    primary key (id)
+);
 
 DROP TABLE IF EXISTS Participa CASCADE;
+CREATE TABLE Participa
+(
+    batalla_clan INTEGER,
+    clan         VARCHAR(50),       /*TODO: tipo?*/
+    fecha_inicio  Date,
+    fech_fin   Date,
+    PRIMARY KEY (batalla_clan, clan)
+);
 
-DROP TABLE IF EXISTS Desbloquea CASCADE;
+Drop table if exists Consigue cascade;
+Create table Consigue
+(
+    id_insignia int,
+    id_arena    int,
+    id_jugador  Varchar(50),
+    fecha       date,
+    primary key (id_insignia, id_arena, id_jugador),
+    foreign key (id_jugador) references Jugador (id),
+    foreign key (id_insignia) references Insignia (id),
+    foreign key (id_arena) references Arena (id)
+);
 
-DROP TABLE IF EXISTS Logro CASCADE;
+Drop table if exists Mensaje_clan cascade;
+Create table Mensaje_clan
+(
+    id                      serial,
+    cuerpo                  text,
+    fecha                   Date,
+    emisor                  int,         /*TODO: tipo?*/
+    receptor                int,         /*TODO: tipo?*/
+    id_mensaje_respondido   int,
+    primary key (id),
+    foreign key (id_mensaje_respondido) references Mensaje_clan (id)
+);
 
-DROP TABLE IF EXISTS Obtiene CASCADE;
+Drop table if exists Tecnologia cascade;
+Create table Tecnologia
+(
+    nombre          Varchar(50),
+    coste_oro       int,
+    descripcion     text,
+    nivel_max       int,
+    dep_level       int,
+    damage          int,        /*TODO: tipo?*/
+    deploy_damage   int,        /*TODO: tipo?*/
+    radio           int,        /*TODO: tipo?*/
+    vel_ataque      int,        /*TODO: tipo?*/
+    vida            int,        /*TODO: tipo?*/
+    tec_dep         int,        /*TODO: tipo?*/
+    primary key (nombre),
+    foreign key (tec_dep) references Modificadors (id) /*TODO: tipo?*/
+);
 
-DROP TABLE IF EXISTS Batalla_Jugadores CASCADE;
+Drop table if exists Estructura cascade;
+Create table Estructura
+(
+    nombre          Varchar(50),
+    coste_oro       int,
+    descripcion     text,
+    trofeos         int,
+    damage          int,        /*TODO: tipo?*/
+    deploy_damage   int,        /*TODO: tipo?*/
+    radio           int,        /*TODO: tipo?*/
+    vel_ataque      int,        /*TODO: tipo?*/
+    vida            int,        /*TODO: tipo?*/
+    tec_dep         int,        /*TODO: tipo?*/
+    primary key (nombre),
+    foreign key (tec_dep) references Modificadors (id) /*TODO: tipo?*/
+);
 
-DROP TABLE IF EXISTS Ocurre CASCADE;
+Drop table if exists Modificador cascade;
+Create table Modificador
+(
+    id_clan         serial,
+    estructura      Varchar(50),
+    tecnologia      text,
+    costeOro        int,
+    nivel           int,
+    fecha           date,
+    primary key (id_clan, estructura, tecnologia),
+    foreign key (id_clan) references Clan (id), /*TODO: tipo?*/
+    foreign key (estructura) references Estructura (nombre), /*TODO: tipo?*/
+    foreign key (tecnologia) references Tecnologia (nombre) /*TODO: tipo?*/
 
-DROP TABLE IF EXISTS Mision CASCADE;
-
-DROP TABLE IF EXISTS Realiza CASCADE;
-
-DROP TABLE IF EXISTS Depende;
-
-drop table if exists Deck cascade;
-
-drop table if exists Ve cascade;
-
-drop table if exists Deck_carta cascade;
-
-drop table if exists Encuentra cascade;
-
-drop table if exists Luchan cascade;
-
-drop table if exists Edificio cascade;
-
-drop table if exists Tropas cascade;
-
-drop table if exists Encantamiento cascade;
-
-Drop table if exists Mensaje cascade;
-
-Drop table if exists Escribe cascade;
-
-Drop table if exists Amigo cascade;
-
-Drop table if exists Tarjeta cascade;
-
-Drop table if exists Posee cascade;
-
-Drop table if exists Articulo cascade;
-
-Drop table if exists Compra cascade;
-
-Drop table if exists Cofre cascade;
-
-Drop table if exists Paquete_Arena cascade;
-
-Drop table if exists Paquete_Oferta cascade;
-
-Drop table if exists Emoticono cascade;
-
-Drop table if exists Contiene cascade;
+);
