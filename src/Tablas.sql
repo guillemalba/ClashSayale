9e114fabe87c2cccac55da4ae01787f4149ad9e6
Guillem Alba <guillemalba@hotmail.com>
2022-03-01 13:49:49 +0100

    Tablas de color verde bien hechas creo, pero no las he testeado.

--- src/Tablas.sql
+++ src/Tablas.sql
@@ -1,9 +1,9 @@
 /*------------ GUILLEM (VERDE)------------*/
-Drop table if exists Jugador cascade;
-Create table Jugador
+drop table if exists Jugador cascade;
+create table Jugador
 (
-    id          Varchar(50),
-    nombre      Varchar(50),
+    id          varchar(255),
+    nombre      varchar(255),
     experiencia int,
     trofeos     int,
     oro         int,
@@ -11,151 +11,151 @@ Create table Jugador
     primary key (id)
 );
 
-Drop Table If Exists Clan Cascade;
-Create Table Clan
+drop table if exists Clan cascade;
+create table Clan
 (
-    id             Varchar(25),
-    nombre         Varchar(50),
-    descripcion    Text,
+    id             varchar(255),
+    nombre         varchar(255),
+    descripcion    text,
     trofeos_totales int,
     minimo_trofeos  int,
     puntuacion     int,
-    Primary Key (id)
+    primary key (id)
 );
 
-Drop table if exists Formado cascade;
-Create table Formado
+drop table if exists Formado cascade;
+create table Formado
 (
-    id_clan     Varchar(50),
-    id_jugador  Varchar(50),
+    id_clan     varchar(255),
+    id_jugador  varchar(255),
     fecha date,
-    role Varchar(50),
+    role varchar(255),
     primary key (id_clan, id_jugador),
     foreign key (id_clan) references Clan (id),
     foreign key (id_jugador) references Jugador (id)
 );
 
-Drop table if exists Dona cascade;
-Create table Dona
+drop table if exists Dona cascade;
+create table Dona
 (
-    id_clan        Varchar(50),
-    id_jugador     Varchar(50),
-    oro           int,
+    id_clan        varchar(255),
+    id_jugador     varchar(255),
+    oro            int,
     fecha          date,
     primary key (id_clan, id_jugador),
     foreign key (id_clan) references Clan (id),
     foreign key (id_jugador) references Jugador (id)
 );
 
-Drop table if exists Insignia cascade;
-Create table Insignia
+drop table if exists Insignia cascade;
+create table Insignia
 (
-    nombre        serial,           /*TODO: tipo?*/
-    imagenUrl     text,
+    nombre        varchar(255),
+    imagen_url     text,
     primary key (nombre)
 );
 
-Drop table if exists Adquiere cascade;
-Create table Adquiere
+drop table if exists Adquiere cascade;
+create table Adquiere
 (
-    id_clan     Varchar(50),
+    id_clan     varchar(255),
     id_insignia int,
     fecha       date,
     primary key (id_clan, id_insignia),
     foreign key (id_clan) references Clan (id),
-    foreign key (id_insignia) references Insignia (id)
+    foreign key (id_insignia) references Insignia (nombre)
 );
 
-Drop table if exists Batalla_Clan cascade;
-Create table Batalla_Clan
+drop table if exists Batalla_Clan cascade;
+create table Batalla_Clan
 (
     id         serial,
     primary key (id)
 );
 
-DROP TABLE IF EXISTS Participa CASCADE;
-CREATE TABLE Participa
+drop table if exists Participa cascade;
+create table Participa
 (
-    batalla_clan INTEGER,
-    clan         VARCHAR(50),       /*TODO: tipo?*/
-    fecha_inicio  Date,
-    fech_fin   Date,
-    PRIMARY KEY (batalla_clan, clan)
+    batalla_clan integer,
+    clan         varchar(255),
+    fecha_inicio  date,
+    fech_fin   date,
+    primary key (batalla_clan, clan)
 );
 
-Drop table if exists Consigue cascade;
-Create table Consigue
+drop table if exists Consigue cascade;
+create table Consigue
 (
     id_insignia int,
     id_arena    int,
-    id_jugador  Varchar(50),
+    id_jugador  varchar(255),
     fecha       date,
     primary key (id_insignia, id_arena, id_jugador),
     foreign key (id_jugador) references Jugador (id),
-    foreign key (id_insignia) references Insignia (id),
+    foreign key (id_insignia) references Insignia (nombre),
     foreign key (id_arena) references Arena (id)
 );
 
-Drop table if exists Mensaje_clan cascade;
-Create table Mensaje_clan
+drop table if exists Mensaje_clan cascade;
+create table Mensaje_clan
 (
     id                      serial,
     cuerpo                  text,
-    fecha                   Date,
-    emisor                  int,         /*TODO: tipo?*/
-    receptor                int,         /*TODO: tipo?*/
+    fecha                   date,
+    emisor                  varchar(255),
+    receptor                varchar(255),
     id_mensaje_respondido   int,
     primary key (id),
     foreign key (id_mensaje_respondido) references Mensaje_clan (id)
 );
 
-Drop table if exists Tecnologia cascade;
-Create table Tecnologia
+drop table if exists Tecnologia cascade;
+create table Tecnologia
 (
-    nombre          Varchar(50),
+    nombre          varchar(255),
     coste_oro       int,
     descripcion     text,
     nivel_max       int,
     dep_level       int,
-    damage          int,        /*TODO: tipo?*/
-    deploy_damage   int,        /*TODO: tipo?*/
-    radio           int,        /*TODO: tipo?*/
-    vel_ataque      int,        /*TODO: tipo?*/
-    vida            int,        /*TODO: tipo?*/
-    tec_dep         int,        /*TODO: tipo?*/
+    damage          int,
+    deploy_damage   int,
+    radio           int,
+    vel_ataque      int,
+    vida            int,
+    tec_dep         varchar(255),
     primary key (nombre),
-    foreign key (tec_dep) references Modificadors (id) /*TODO: tipo?*/
+    foreign key (tec_dep) references Tecnologia (nombre)
 );
 
-Drop table if exists Estructura cascade;
-Create table Estructura
+drop table if exists Estructura cascade;
+create table Estructura
 (
-    nombre          Varchar(50),
+    nombre          varchar(255),
     coste_oro       int,
     descripcion     text,
     trofeos         int,
-    damage          int,        /*TODO: tipo?*/
-    deploy_damage   int,        /*TODO: tipo?*/
-    radio           int,        /*TODO: tipo?*/
-    vel_ataque      int,        /*TODO: tipo?*/
-    vida            int,        /*TODO: tipo?*/
-    tec_dep         int,        /*TODO: tipo?*/
+    damage          int,
+    deploy_damage   int,
+    radio           int,
+    vel_ataque      int,
+    vida            int,
+    tec_dep         varchar(255),
     primary key (nombre),
-    foreign key (tec_dep) references Modificadors (id) /*TODO: tipo?*/
+    foreign key (tec_dep) references Estructura (nombre)
 );
 
-Drop table if exists Modificador cascade;
-Create table Modificador
+drop table if exists Modificador cascade;
+create table Modificador
 (
-    id_clan         serial,
-    estructura      Varchar(50),
-    tecnologia      text,
+    id_clan         varchar(255),
+    estructura      varchar(255),
+    tecnologia      varchar(255),
     costeOro        int,
     nivel           int,
     fecha           date,
     primary key (id_clan, estructura, tecnologia),
-    foreign key (id_clan) references Clan (id), /*TODO: tipo?*/
-    foreign key (estructura) references Estructura (nombre), /*TODO: tipo?*/
-    foreign key (tecnologia) references Tecnologia (nombre) /*TODO: tipo?*/
+    foreign key (id_clan) references Clan (id),
+    foreign key (estructura) references Estructura (nombre),
+    foreign key (tecnologia) references Tecnologia (nombre)
 
 );
