d4063e6946ca6e359c82a061df953f721356c9cc
dserrabernad <d98serra@gmail.com>
2022-03-15 19:03:26 +0100

    Primeras queries de fase 3

---
+++ src/fase3Queries.sql
@@ -0,0 +1,152 @@
+/***************** APARTADO 1 *****************/
+/* 1.1
+ * Enumerar el nom i el valor de dany de les cartes de tipus de tropa amb un valor de
+ * velocitat d’atac superior a 100 i el nom del qual contingui el caràcter "k".
+ */
+select c.nombre, c.daño
+from carta c join tropas t on c.id = t.carta
+where velocidad_ataque > 100 and c.nombre like '%k%';
+
+
+/* 1.2
+ * Enumerar el valor de dany mitjà, el valor de dany màxim i el valor de dany mínim de les
+ * cartes èpiques.
+ */
+select avg(daño) as daño_medio, max(daño) as daño_max, min(daño) as daño_min
+from carta
+where rareza = 'Epic'
+group by rareza;
+
+
+/* 1.3
+ * Enumera el nom i la descripció de les piles i el nom de les cartes que tenen un nivell de
+ * carta més gran que el nivell mitjà de totes les cartes. Ordena els resultats segons el nom
+ * de les piles i el nom de les cartes de més a menys valor.
+ */
+select d.titulo, d.descripcion, c2.nombre, c.nivel
+from deck d join compuesto c on d.id = c.deck
+join carta c2 on c.carta = c2.id
+where c.nivel > (select avg(nivel) from compuesto)
+order by d.titulo desc, c2.nombre desc;
+
+
+
+
+/* 1.4
+ * Enumerar el nom i el dany de les cartes llegendàries de menor a major valor de dany
+ * que pertanyin a una pila creada l'1 de novembre del 2021. Filtrar la sortida per tenir les
+ * deu millors cartes.
+ */
+select c.nombre, c.daño
+from carta c join compuesto c2 on c.id = c2.carta
+join deck d on c2.deck = d.id
+where c.rareza = 'Epic' and d.fecha = '2021-11-01'
+group by c.id, c.daño, c.nombre
+order by c.daño desc
+limit 10;
+
+
+/* 1.5
+ * Llistar les tres primeres cartes de tipus edifici (nom i dany) en funció del dany dels
+ * jugadors amb experiència superior a 250.000.
+ * Base de dades: 2021-2022 Projecte – Fase 3
+ */
+select c.nombre, c.daño
+from edificio e join carta c on c.id = e.carta
+join compuesto c2 on c.id = c2.carta
+join deck d on c2.deck = d.id
+join jugador j on d.jugador = j.id
+where j.experiencia > 250000
+group by c.nombre, c.daño
+order by c.daño desc
+limit 3;
+
+
+/* 1.6
+ * Els dissenyadors del joc volen canviar algunes coses a les dades. El nom d'una carta
+ * "Rascals" serà "Hal Roach's Rascals", la Raresa "Common" es dirà "Proletari".
+ * Proporcioneu les ordres SQL per fer les modificacions sense eliminar les dades i
+ * reimportar-les.
+ */
+
+
+
+/* 1.7
+ * Enumerar els noms de les cartes que no estan en cap pila i els noms de les cartes que
+ * només estan en una pila. Per validar els resultats de la consulta, proporcioneu dues
+ * consultes diferents per obtenir el mateix resultat.
+ */
+
+
+
+/* 1.8
+ * Enumera el nom i el dany de les cartes èpiques que tenen un dany superior al dany mitjà
+ * de les cartes llegendàries. Ordena els resultats segons el dany de les cartes de menys
+ * a més valor.
+ */
+
+select nombre, daño
+from carta
+where rareza = 'Epic' and daño > (select avg(daño) from carta where rareza = 'Legendary')
+order by daño;
+
+
+
+
+/***************** APARTADO 1 *****************/
+/* 2.1
+ * Enumera els missatges (text i data) escrits pels jugadors que tenen més experiència que
+ * la mitjana dels jugadors que tenen una "A" en nom seu i pertanyen al clan "NoA". Donar
+ * la llista de ordenada dels missatges més antics als més nous.
+ */
+
+
+/* 2.2
+ * Enumera el número de la targeta de crèdit, la data i el descompte utilitzat pels jugadors
+ * per comprar articles de Pack Arena amb un cost superior a 200 i per comprar articles
+ * que el seu nom contingui una "B".
+ */
+
+
+
+/* 2.3
+ * Enumerar el nom i el nombre d’articles comprats, així com el cost total dels articles
+ * comprats i l’experiència dels jugadors que els van demanar. Filtra la sortida amb els 5
+ * articles en què els usuaris han gastat més diners.
+ */
+
+
+
+/* 2.4
+ * Donar els números de les targetes de crèdit que s'han utilitzat més.
+ */
+
+
+
+/* 2.5
+ * Donar els descomptes totals de les emoticones comprades durant l'any 2020
+ */
+
+
+
+/* 2.6
+ * Enumerar el nom, experiència i número de targeta de crèdit dels jugadors amb
+ * experiència superior a 150.000. Filtra les targetes de crèdit que no han estat utilitzades
+ * per comprar cap article. Donar dues consultes diferents per obtenir el mateix resultat.
+ */
+
+
+
+/* 2.7
+ * Retorna el nom dels articles comprats pels jugadors que tenen més de 105 cartes o pels
+ * jugadors que han escrit més de 4 missatges. Ordeneu els resultats segons el nom de
+ * l'article de més a menys valor.
+ */
+
+
+
+/*  2.8
+ * Retorna els missatges (text i data) enviats a l'any 2020 entre jugadors i que hagin estat
+ * respostos, o els missatges sense respostes enviats a un clan. Ordena els resultats
+ * segons la data del missatge i el text del missatge de més a menys valor.
+ */
